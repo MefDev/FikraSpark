3014505a18d8180947ce90d8fced85688e8789e9
Mefdev <mefdev.pro@gmail.com>
2025-08-02 15:50:45 +0100

    Fix: enhance idea entity to have vote cababilties

--- FikraSparkCore/src/Domain/Entities/Idea.cs
+++ FikraSparkCore/src/Domain/Entities/Idea.cs
@@ -4,16 +4,19 @@ public class Idea: BaseAuditableEntity
 {
     public string Title { get; private set; }
     public string Description { get; private set; }
+    public int Votes { get; set; } = 0;
 
-    public Idea(string title, string description)
+    public Idea(string title, string description, int votes)
     {
         Title = title;
         Description = description;
+        Votes = votes;
     }
     
-    public void Update(string title, string description)
+    public void Update(string title, string description, int votes)
     {
         Title = title;
         Description = description;
+        Votes = votes;
     }
 }
